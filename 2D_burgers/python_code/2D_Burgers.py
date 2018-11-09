a536de9dbed690f6a3d8db614e76c0054887cde1
Marcus <mpereira30.markper4190@gmail.com>
2018-11-09 13:44:03 -0500

    updates

--- 2D_burgers/python_code/2D_Burgers.py
+++ 2D_burgers/python_code/2D_Burgers.py
@@ -7,9 +7,9 @@ import sys
 import matplotlib.colors as colors
 
 ###variable declarations
-nx = 41
-ny = 41
-nt = 1000
+nx = 30
+ny = 30
+nt = 200
 c  = 1
 a  = 2
 
@@ -19,8 +19,15 @@ dy = a / (ny - 1)
 sigma = .2
 
 nu = 0.1
-dt = sigma * dx * dy / nu
-print("\n time discretization:",dt)
+# dt = sigma * dx * dy / nu
+dt = 0.01
+print("Parameters used:")
+print("nu (viscoity):", nu)
+print("time discretization:",dt)
+print("r=", nu*dt/(dx**2)) 
+print("")
+
+# r is directly proportional to nx or ny
 
 x = np.linspace(0, a, nx)
 y = np.linspace(0, a, ny)
@@ -44,6 +51,15 @@ u[int(.75 / dy):int(1.25 / dy + 1),int(1.25 / dx):int(1.75 / dx + 1)] = -2.0
 # set hat function I.C. : u(.5<=x<=1 && .5<=y<=1 ) is 2
 # v[int(.25 / dy):int(1 / dy + 1),int(.5 / dx):int(1 / dx + 1)] = 2
 
+# for i in range(nx):
+# 	for j in range(ny):
+# 		if (y[j]>=0.5 and y[j]<=1.0) and (x[i]>=0.5 and x[i]<=1.0):
+# 			u[i,j] = 0.0
+# 			v[i,j] = 1.0
+# 		else:
+# 			u[i,j] = 1.0
+# 			v[i,j] = 0.0
+
 all_us = np.zeros((nt+1,ny,nx))
 all_vs = np.zeros((nt+1,ny,nx))
 all_us[0,:,:] = u
@@ -54,6 +70,8 @@ for n in range(nt): ##loop across number of time steps
 	vn = v.copy()
 
 	if use_backward_diff:
+		if n == 0:
+			print("Using backward difference for advection")
 		u[1:-1, 1:-1] = (un[1:-1, 1:-1] -
 		                 dt / dx * un[1:-1, 1:-1] * 
 		                 (un[1:-1, 1:-1] - un[1:-1, 0:-2]) - 
@@ -74,6 +92,8 @@ for n in range(nt): ##loop across number of time steps
 		                 nu * dt / dy**2 *
 		                 (vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[0:-2, 1:-1]))
 	elif use_central_diff:
+		if n == 0:
+			print("Using backward difference for advection")		
 		u[1:-1, 1:-1] = (un[1:-1, 1:-1] -
 		                 (0.5*dt / dx) * un[1:-1, 1:-1] * 
 		                 (un[1:-1, 2:] - un[1:-1, 0:-2]) - 
@@ -142,7 +162,7 @@ for n in range(nt):
 	# if n==0:
 	# 	plt.colorbar();
 	# Q = plt.quiver(X,Y,curr_u, curr_v, units='xy', angles='xy', scale_units='xy', scale=10, color='black')		
-	Q = plt.quiver(X,Y,curr_u, curr_v, Z, cmap='autumn', clim= [vmin, vmax], units='xy', angles='xy', scale_units='xy', scale=10)
+	Q = plt.quiver(X,Y,curr_u, curr_v, Z, cmap='autumn', clim= [vmin, vmax], units='xy', angles='xy', scale_units='xy', scale=15)
 	if n==0:
 		plt.colorbar(Q,extend='max');		
 
